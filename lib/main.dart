d848f9227a23c405f33983a5445031e2f0e31238
ivanruzin <ruzinivan1@gmail.com>
2024-10-05 01:18:35 +0300

    task4 mirea part1 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:family_locator/features/profile/presentation/profile_screen.dart';
+import 'package:family_locator/core/presentation/task4_listview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,44 +17,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProfileScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "ФИО: Рузин Иван Александрович",
-            ),
-            Text(
-              "Учебная группа: ИКБО-06-21",
-            ),
-            Text(
-              "Номер студенческого: 21И1786",
-            ),
-          ],
-        ),
-      ),
+      //home: ProfileScreen(),
+      home: const Task4ListView(),
     );
   }
 }
